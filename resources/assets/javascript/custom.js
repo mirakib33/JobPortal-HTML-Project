bf2a63ede7e77d496458846806ec93ea4175b6a2
Mazharul Islam Rakib <106418131+mirakib33@users.noreply.github.com>
2023-01-14 23:49:45 +0600

    home

--- resources/assets/javascript/custom.js
+++ resources/assets/javascript/custom.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     })
 });
 
-// Tabs start here
+// Tabs ends here
 
 // accordions start here
 
@@ -83,3 +83,42 @@ $(".save-btn").click(function(){
   $(".container .academic-summary").show();
   
 });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+   
+  const showNavbar = (toggleId, navId, divId, headerId) =>{
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId),
+  bodypd = document.getElementById(divId),
+  headerpd = document.getElementById(headerId)
+  
+  // Validate that all variables exist
+  if(toggle && nav && bodypd && headerpd){
+  toggle.addEventListener('click', ()=>{
+  // show navbar
+  nav.classList.toggle('show')
+  // change icon
+  toggle.classList.toggle('bx-x')
+  // add padding to body
+  bodypd.classList.toggle('body-pd')
+  // add padding to header
+  headerpd.classList.toggle('body-pd')
+  })
+  }
+  }
+  
+  showNavbar('header-toggle','nav-bar','body-pd','header')
+  
+  /*===== LINK ACTIVE =====*/
+  const linkColor = document.querySelectorAll('.nav_link')
+  
+  function colorLink(){
+  if(linkColor){
+  linkColor.forEach(l=> l.classList.remove('active'))
+  this.classList.add('active')
+  }
+  }
+  linkColor.forEach(l=> l.addEventListener('click', colorLink))
+  
+   // Your code to run since DOM is loaded and ready
+  });
